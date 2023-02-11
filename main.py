a36f4959f5ea90ca2041af9874edc1750bf13867
Matteo <matteorascioni@gmail.com>
2023-02-11 20:42:17 +0100

    python oop quiz game

---
+++ main.py
@@ -0,0 +1,19 @@
+from data import question_data
+from quiz_brain import QuizBrain
+from question_model import Question
+
+question_bank = []
+
+for q in question_data:
+    question_text = q["question"]
+    question_answer = q["correct_answer"]
+    new_question = Question(text=question_text, answer=question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
